045a8011f2705c83ababed8503ee324b6ad14a0d
Paul Vasich <paul.vasich@gmail.com>
2018-07-04 16:07:56 -0500

    more donorquotes

--- donor-quotes.js
+++ donor-quotes.js
@@ -31,4 +31,100 @@ export default [
     'Gravetch',
     'June 19, 2017',
   ],
+  [
+    `This is such a nice stream. Very entertaining!
+    Will be tuning in more times :D`,
+    'sofiainha39',
+    '2017',
+  ],
+  [
+    'Your streams help me get through the day',
+    'solo_zyt',
+    '2017',
+  ],
+  [
+    'Love the stream! happy to join the journey of your story ^^',
+    'nagyandy92',
+    '2017',
+  ],
+  [
+    '^^^^^ Carrot. ^^ But also carrot, and care it. And carrot.',
+    'whatupitsyaboyspagolio',
+    '2017',
+  ],
+  [
+    `For dealing with me and all my mental fun issues!!! Thanks Rook, you're a pretty amazing person!!`,
+    'Kiyin84',
+    '2017',
+  ],
+  [
+    `Been having a lot of fun hanging in your stream for the past few days. Thanks for being so welcoming!`,
+    'Raines2382',
+    '2017',
+  ],
+  [
+    `First day seeing you stream. But already enjoying it. Help me with my Gambling Addiction doe D=`,
+    'Spaceexza',
+    '2017',
+  ],
+  [
+    `Figured I could do this cause of how much I enjoy the streams. Keep it up Rook!!!!`,
+    'mineanation',
+    '2017',
+  ],
+  [
+    `Your stream brought me back to FFXIV. Thank you for being so positive and fun!`,
+    'ceaselessbarlow',
+    '2017',
+  ],
+  [
+    `For the singing! ...and Frog Flare. I've heard it's a good attack.`,
+    'wardamnalto',
+    '2017',
+  ],
+  [
+    `You are great, keep on going!!`,
+    'awkwardghost',
+    '2017',
+  ],
+  [
+    `Ding, its the sound you hear when rook gets a well deserved donation! Yes, that donation amount is not an accident.`,
+    'evenmadderscientist',
+    '2017',
+  ],
+  [
+    `Dedicated to the chips Ahoy cookies I am eating currently and tambien para tu being a amable persona. (Part of this is in spanish. Translate.)`,
+    'jessthetess',
+    '2017',
+  ],
+  [
+    `Ding, its that sound you hear when Rook gets another well deserved donation! With our anima weapons almost done, we shall rule the whole galaxy of Eorzea! This donation is for the recent awesome streams~ Keep it going, fellow stargazer!`,
+    'evenmadderscientist',
+    '2017',
+  ],
+  [
+    `I would also love to chime in with the support! I really like your attitude and I think you are amazing :D You literally made my day yesterday and today it was so nice to see you online once I was back home from uni! <3 <3 <3`,
+    'bomz',
+    '2017',
+  ],
+  [
+    `Entertaining AF, hope more ppl support u!`,
+    'ugghthatguy',
+    '2017',
+  ],
+  [
+    `Great choice of Games, Great Streamer, and Great Content. This is for the good work. Keep it up! Can't wait to see more!`,
+    'saintjames',
+    '2017',
+  ],
+  [
+    'Thanks for the impressions and being a bigger nerd than me.',
+    'flownominal_15',
+    '2017',
+  ],
+  [
+    '#KeepRookCawing',
+    'benaveil',
+    '2017',
+  ],
 ];
