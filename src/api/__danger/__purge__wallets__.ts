78089382cd626db67527e4bbae8f8ef85d326b2e
Paul Vasich <paul.vasich@gmail.com>
2019-04-25 11:18:32 -0500

    forgot to write

--- src/api/__danger/__purge__wallets__.ts
+++ src/api/__danger/__purge__wallets__.ts
@@ -12,7 +12,7 @@ export function __purge__wallets__() {
     try {
         db.get(MODELS.TICKETS).keys().forEach(function (key) {
             db.get(MODELS.TICKETS).set(key, emptyStore()).write();
-        });
+        }).write();
         return '200::Everything is gone. The world is barren. What have I done.'
     } catch (e) {
         return '500::Ow, my brain'
