d0fafbb67c4aa173d97e65e9a57021c3e6bc816c
Paul Vasich <paul.vasich@gmail.com>
2021-01-01 17:14:17 -0600

    feat(public announce)

--- src/api/channel-points/draw.ts
+++ src/api/channel-points/draw.ts
@@ -1,6 +1,7 @@
 import db from '../../db';
 import { Model } from '../../db/db-constants';
 import { ChannelPointReward } from '../../bot/rewards';
+import client from '../../bot/client';
 
 
 export default function draw(reward: ChannelPointReward, winnerCount: number = 1) {
@@ -8,6 +9,8 @@ export default function draw(reward: ChannelPointReward, winnerCount: number = 1
     const winners: string[] = [];
     const value = db.get(Model.Channel_Points).value();
     const keys = Object.keys(value);
+
+    /** simulate an actual raffle drawing, for fun */
     for (let i = 0, n = keys.length; i < n; i++) {
         const key = keys[i];
         const entry = value[key];
@@ -19,11 +22,43 @@ export default function draw(reward: ChannelPointReward, winnerCount: number = 1
         }
     }
 
+    if (winnerCount > 1) {
+        client.say('birdofchess', 'The winners are:');
+    } else {
+        client.say('birdofchess', 'The winner is:');
+    }
+    /** splice out winners to keep odds fair */
     for (let i = 0, n = winnerCount; i < n; i++) {
         const windex = Math.floor(Math.random() * entries.length);
         const [winner] = entries.splice(windex, 1);
-        winners.push(winner);
+        let pre = '';
+        if (winnerCount > 1) {
+            pre = `#${i + 1}: `;
+        }
+        const winningUserDisplayName = db
+            .get(Model.Channel_Points)
+            .get(winner)
+            .get('userDisplayName')
+            .value()
+
+        const result = `${pre}@${winningUserDisplayName}!`;
+
+        winners.push(result);
+
+        setTimeout(() => {
+            client.say(
+                'birdofchess',
+                result,
+            );
+        }, 1000);
+
+        if ((i + 1) === winnerCount) {
+            setTimeout(() => {
+                client.say('birdofchess', 'Congratulations!');
+            }, 1000);
+        }
     }
 
-    console.log(winners.map((winner) => db.get(Model.Channel_Points).get(winner).get('userDisplayName').value()));
+    /** just in case */
+    console.log(winners);
 }
